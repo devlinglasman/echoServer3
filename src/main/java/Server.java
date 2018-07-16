dcd18a5a48c9c82dfe74757701162ca5c84ab28f
Devlin Glasman <devlin.glasman@gmail.com>
2018-07-16 11:13:52 +0100

    Adds test so that after connection, server sends confirmation message to client.

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -28,4 +28,8 @@ public class Server {
             e.printStackTrace();
         }
     }
+
+    public Socket getClientSocket() {
+        return clientSocket;
+    }
 }
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,4 +1,5 @@
 import com.sun.xml.internal.bind.v2.util.ByteArrayOutputStreamEx;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
@@ -8,21 +9,46 @@ import static junit.framework.TestCase.assertEquals;
 
 public class ServerTest {
 
+    private InputStream stdIn = new ByteArrayInputStream("".getBytes());
+    private ByteArrayOutputStream stdOut = new ByteArrayOutputStream();
+    private PrintStream stdPrint = new PrintStream(stdOut);
+
+    private InputStream dataIntoServerSocket = new ByteArrayInputStream("".getBytes());
+    private OutputStream dataOutOfServerSocket = new ByteArrayOutputStream();
+
+    private SocketStub socketStub = new SocketStub(dataIntoServerSocket, dataOutOfServerSocket);
+    private ServerSocketStub serverSocketStub = new ServerSocketStub(socketStub);
+    private Server server = new Server(stdIn, stdPrint, serverSocketStub);
+
+    public ServerTest() throws IOException {
+    }
+
+    @Before
+    public void setup() throws IOException {
+        stdIn = new ByteArrayInputStream("".getBytes());
+        stdOut = new ByteArrayOutputStream();
+        stdPrint = new PrintStream(stdOut);
+
+        dataIntoServerSocket = new ByteArrayInputStream("".getBytes());
+        dataOutOfServerSocket = new ByteArrayOutputStream();
+
+        socketStub = new SocketStub(dataIntoServerSocket, dataOutOfServerSocket);
+        serverSocketStub = new ServerSocketStub(socketStub);
+        server = new Server(stdIn, stdPrint, serverSocketStub);
+    }
+
+
     @Test
-    public void serverAcceptsClient() throws IOException {
-        InputStream stdIn = new ByteArrayInputStream("".getBytes());
-        ByteArrayOutputStream stdOut = new ByteArrayOutputStream();
-        PrintStream stdPrint = new PrintStream(stdOut);
-
-        InputStream dataIntoServerSocket = new ByteArrayInputStream("".getBytes());
-        OutputStream dataOutOfServerSocket = new ByteArrayOutputStream();
-
-        SocketStub socketStub = new SocketStub(dataIntoServerSocket, dataOutOfServerSocket);
-        ServerSocketStub serverSocketStub = new ServerSocketStub(socketStub);
-        Server server = new Server(stdIn, stdPrint, serverSocketStub);
-
+    public void serverAcceptsClient_printsToTerminal() {
         server.start();
 
         assertEquals(Message.clientConnected + "\n", stdOut.toString());
+    }
+
+    @Test
+    public void serverAcceptsClient_sendsConfirmationToClient() throws IOException {
+        server.start();
+
+        assertEquals(Message.clientConnected + "\n", server.getClientSocket().getOutputStream().toString());
     }
 }
